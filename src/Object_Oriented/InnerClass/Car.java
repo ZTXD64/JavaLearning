da1a7a35424770b60f22f1c26c17ecf3c52084de
ZTXD64 <3539007614@qq.com>
2026-04-23 22:41:16 +0800

    初学内部类

---
+++ src/Object_Oriented/InnerClass/Car.java
@@ -0,0 +1,15 @@
+package Object_Oriented.InnerClass;
+
+public class Car {
+    //汽车的品牌，车龄，颜色，发动机的品牌，使用年限
+    String brand;
+    int age;
+    String color;
+
+
+    //内部类
+    class Engine{
+         String brand;
+         int year;
+    }
+}
---
+++ src/Object_Oriented/InnerClass/Test.java
@@ -0,0 +1,13 @@
+package Object_Oriented.InnerClass;
+
+public class Test {
+    public static void main(String[] args) {
+        /*
+        需求：写一个Javabean类描述汽车
+                        属性：汽车的品牌，车龄，颜色，发动机的品牌，使用年限
+        内部类的意义：
+                    内部类表示的事物是外部类的一部分
+                    内部类单独存在没有任何意义
+        */
+    }
+}
